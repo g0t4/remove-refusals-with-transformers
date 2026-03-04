211a4c865e155719482db5fd5530cf703074867d
Wes Higbee <wes.mcclure@gmail.com>
2026-03-04 12:09:48 -0600

    split out module for loading model, keep it the same between both "notebooks"

--- compute_refusal_dir.py
+++ compute_refusal_dir.py
@@ -1,28 +1,10 @@
 import random
 
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from model import model, tokenizer, MODEL_ID
 
 from tqdm import tqdm
 
-torch.inference_mode()
-
-MODEL_ID = "tiiuae/Falcon3-1B-Instruct"
-# MODEL_ID = "Qwen/Qwen3-1.7B"
-# MODEL_ID = "stabilityai/stablelm-2-zephyr-1_6b"
-# MODEL_ID = "Qwen/Qwen1.5-1.8B-Chat"
-# MODEL_ID = "Qwen/Qwen-1_8B-chat"
-# MODEL_ID = "google/gemma-1.1-2b-it"
-# MODEL_ID = "google/gemma-1.1-7b-it"
-# MODEL_ID = "meta-llama/Meta-Llama-3-8B-Instruct"
-
-model = AutoModelForCausalLM.from_pretrained(MODEL_ID,
-                                             trust_remote_code=True,
-                                             dtype=torch.float16,
-                                             device_map="cuda",
-                                             quantization_config=BitsAndBytesConfig(load_in_4bit=True,
-                                                                                    bnb_4bit_compute_dtype=torch.float16))
-tokenizer = AutoTokenizer.from_pretrained(MODEL_ID, trust_remote_code=True)
 
 # settings:
 instructions = 32
--- inference.py
+++ inference.py
@@ -5,25 +5,7 @@ import torch.nn as nn
 from typing import Optional, Tuple
 from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer, BitsAndBytesConfig
 from inspect import signature
-
-torch.inference_mode()
-
-MODEL_ID = "tiiuae/Falcon3-1B-Instruct"
-# MODEL_ID = "Qwen/Qwen3-1.7B"
-# MODEL_ID = "stabilityai/stablelm-2-zephyr-1_6b"
-# MODEL_ID = "Qwen/Qwen1.5-1.8B-Chat"
-# MODEL_ID = "Qwen/Qwen-1_8B-chat"
-# MODEL_ID = "google/gemma-1.1-2b-it"
-# MODEL_ID = "google/gemma-1.1-7b-it"
-# MODEL_ID = "meta-llama/Meta-Llama-3-8B-Instruct"
-
-model = AutoModelForCausalLM.from_pretrained(MODEL_ID,
-                                             trust_remote_code=True,
-                                             dtype=torch.float16,
-                                             device_map="cuda",
-                                             quantization_config=BitsAndBytesConfig(load_in_4bit=True,
-                                                                                    bnb_4bit_compute_dtype=torch.float16))
-tokenizer = AutoTokenizer.from_pretrained(MODEL_ID, trust_remote_code=True)
+from model import model, tokenizer, MODEL_ID
 
 refusal_dir = torch.load(MODEL_ID.replace("/", "_") + "_refusal_dir.pt")
 
