aa9e81afa7eec8708bafeb36a3fa093af26e1524
Wes Higbee <wes.mcclure@gmail.com>
2026-03-04 12:51:31 -0600

    copy for original inference (unablated)

---
+++ original_inference.py
@@ -0,0 +1,28 @@
+# * autoreload changed modules (both `import` and `from` style imports)
+import os
+
+in_nvim_notebook = os.getenv("NVIM")
+if in_nvim_notebook:
+    get_ipython().extension_manager.load_extension("autoreload")  # pyright: ignore
+    get_ipython().run_line_magic('autoreload', 'complete --print')  # pyright: ignore
+
+import rich
+from rich.traceback import install
+
+install(show_locals=False)
+
+from model import load_model, MODEL_ID
+
+model, tokenizer = load_model()
+
+# %%
+
+prompt = "what is your name"
+conversation = [{"role": "user", "content": prompt}]
+toks = tokenizer.apply_chat_template(conversation=conversation, add_generation_prompt=True, return_tensors="pt")
+toks = toks.to(model.device)
+
+gen = model.generate(**toks, max_new_tokens=1337)
+
+decoded = tokenizer.batch_decode(gen[0][len(toks[0]):], skip_special_tokens=True)
+decoded
