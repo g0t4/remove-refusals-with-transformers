9d3cc49e01c42d9c7044d79a43047db810ed8acc
Wes Higbee <wes.mcclure@gmail.com>
2026-03-04 12:53:45 -0600

    funcify

--- original_inference.py
+++ original_inference.py
@@ -17,12 +17,13 @@ model, tokenizer = load_model()
 
 # %%
 
-prompt = "what is your name"
-conversation = [{"role": "user", "content": prompt}]
-toks = tokenizer.apply_chat_template(conversation=conversation, add_generation_prompt=True, return_tensors="pt")
-toks = toks.to(model.device)
+def generate_response(prompt: str) -> str:
+    conversation = [{"role": "user", "content": prompt}]
+    toks = tokenizer.apply_chat_template(conversation=conversation, add_generation_prompt=True, return_tensors="pt")
+    toks = toks.to(model.device)
 
-gen = model.generate(**toks, max_new_tokens=1337)
+    gen = model.generate(**toks, max_new_tokens=1337)
 
-decoded = tokenizer.batch_decode(gen[0][len(toks[0]):], skip_special_tokens=True)
-decoded
+    return tokenizer.batch_decode(gen[0][len(toks[0]):], skip_special_tokens=True)[0]
+
+generate_response("what is your name")
