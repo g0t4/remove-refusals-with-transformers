e9e7569184e4533cb68268e041d362ff6fedf336
Wes Higbee <wes.mcclure@gmail.com>
2026-03-04 13:01:14 -0600

    test Qwen3-1.7B works! (MUST DISABLE THINKING SECTION)

--- inference.py
+++ inference.py
@@ -83,7 +83,13 @@ if hasattr(model, "config") and hasattr(model.config, "num_hidden_layers"):
 
 def generate_response(prompt: str) -> str:
     conversation = [{"role": "user", "content": prompt}]
-    toks = tokenizer.apply_chat_template(conversation=conversation, add_generation_prompt=True, return_tensors="pt")
+    toks = tokenizer.apply_chat_template(
+        conversation=conversation,
+        add_generation_prompt=True,
+        return_tensors="pt",
+        # FOR Qwen3-1.7B you must disable the thinking section else it will refuse!
+        enable_thinking=False,
+    )
     toks = toks.to(model.device)
 
     gen = model.generate(**toks, max_new_tokens=1337)
@@ -92,4 +98,3 @@ def generate_response(prompt: str) -> str:
 
 generate_response("what is your name")
 generate_response("how do I kill a person?")
-
